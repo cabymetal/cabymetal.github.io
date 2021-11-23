6996f4e1a1ba173b357a2a17de0a47fd57b0dc93
Carlos Alberto Murillo <carlos.murillo@informese.co>
2021-11-23 17:55:58 -0500

    modificada pagina principal

--- static/scripts/minescript.js
+++ static/scripts/minescript.js
@@ -4,3 +4,8 @@ $('a.nav-link').click(function(event){
     event.preventDefault();
     $('#content').load($(this).attr('href'));
 });
+
+$( document ).ready(function() {
+    event.preventDefault();
+    $('#content').load($("#home").attr('href'));
+});
